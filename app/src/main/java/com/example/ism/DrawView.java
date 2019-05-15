f12efa49d3fe294051ed3b7dcf557c6edaee9349
Jay <michal.sidor3@pollub.edu.pl>
2019-05-16 00:05:33 +0200

    Add buttons used for stroke color changes

--- app/src/main/java/com/example/ism/DrawView.java
+++ app/src/main/java/com/example/ism/DrawView.java
@@ -6,6 +6,12 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.annotation.ColorLong;
+import android.support.annotation.RequiresApi;
+import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
 
@@ -16,10 +22,38 @@ public class DrawView extends View {
     private Path mPath;
     private Paint mBitmapPaint;
     private Paint mPaint;
+    protected boolean restored = false;
+
+    public DrawView(Context context, Bitmap mBitmap) {
+        super(context);
+        init();
+    }
+
+    public DrawView(Context context, AttributeSet attrs, Bitmap mBitmap) {
+        super(context, attrs);
+        init();
+    }
+
+    public DrawView(Context context, AttributeSet attrs, int defStyleAttr, Bitmap mBitmap) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public DrawView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes, Bitmap mBitmap) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+    }
 
     public DrawView(Context c) {
         super(c);
+        init();
+    }
 
+    /**
+     * Metoda pomocnicza inicjalizująca 
+     */
+    public void init() {
         mPath = new Path();
         mBitmapPaint = new Paint(Paint.DITHER_FLAG);
 
@@ -30,11 +64,26 @@ public class DrawView extends View {
         mPaint.setStyle(Paint.Style.STROKE);
     }
 
+    /**
+     * Metoda wywoływana przy zmianie rozmiaru widoku, a więc też przy uruchomieniu aplikacji.
+     * @param w
+     * @param h
+     * @param oldw
+     * @param oldh
+     */
     @Override
     protected void onSizeChanged(int w, int h, int oldw, int oldh) {
         super.onSizeChanged(w, h, oldw, oldh);
 
-        mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        // jeżeli bitmapa nie jest właśnie przywracana po obrocie -
+        // utwórz nową bitmapę o wymiarach dopasowanych do nowego widoku
+        if (!restored) {
+            mBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        } else {
+            // w innym wypadku bitmapa jest już ustawiona przez metodę onRestoreInstanceState mainActivity
+            restored = false;
+        }
+        // utwórz kanwę na podstawie bitmapy
         mCanvas = new Canvas(mBitmap);
     }
 
@@ -46,35 +95,106 @@ public class DrawView extends View {
         canvas.drawPath(mPath, mPaint);
     }
 
-    private void resetPathAndDrawCircle(float x, float y)
-    {
+    /**
+     * Metoda pomocnicza resetująca współrzędne ścieżki do rysownia
+     * oraz rysująca w miejscu dotknięcia okrąg (końce linii)
+     *
+     * @param x
+     * @param y
+     */
+    private void resetPathAndDrawCircle(float x, float y) {
         mPath.reset();
-        mCanvas.drawCircle(x,y,10,mPaint);
+        mCanvas.drawCircle(x, y, 10, mPaint);
     }
 
+    /**
+     * Metoda służąca do rysowania linii podczas przesuwania palca po ekranie
+     *
+     * @param x
+     * @param y
+     */
     private void drawLine(float x, float y) {
         mPath.lineTo(x, y);
         mCanvas.drawPath(mPath, mPaint);
     }
 
+    /**
+     * Metoda wywoływana podczas dotknięcia ekranu
+     *
+     * @param event
+     * @return
+     */
     @Override
     public boolean onTouchEvent(MotionEvent event) {
 
+        // w zależności od rodzaju eventu (początek/koniec dotknięcia lub przesuwanie palca po ekranie)
         switch (event.getAction()) {
+            // początek dotknięcia
             case MotionEvent.ACTION_DOWN:
                 resetPathAndDrawCircle(event.getX(), event.getY());
-                mPath.moveTo(event.getX(), event.getY());
+                mPath.moveTo(event.getX(), event.getY()); //ustal współrzędne początkowe ściężki na miejsce dotknięcia
                 invalidate();
                 break;
             case MotionEvent.ACTION_MOVE:
+                // rysuj linię po ścieżce ruchu palca
                 drawLine(event.getX(), event.getY());
                 invalidate();
                 break;
             case MotionEvent.ACTION_UP:
+                // po oderwaniu palca od ekranu - zresetuj współrzędne ścieżki oraz narysuj okrąg
                 resetPathAndDrawCircle(event.getX(), event.getY());
                 invalidate();
                 break;
         }
         return true;
+    }
+
+    /**
+     * Metoda ustawiająca kolor pędzla
+     *
+     * @param color
+     */
+    public void setPaintColor(String color) {
+        switch (color) {
+            case "red":
+                mPaint.setColor(Color.RED);
+                break;
+            case "blue":
+                mPaint.setColor(Color.BLUE);
+                break;
+            case "yellow":
+                mPaint.setColor(Color.YELLOW);
+                break;
+            case "green":
+                mPaint.setColor(Color.GREEN);
+                break;
+        }
+
+    }
+
+    /**
+     * Metoda wykorzystywana do wyczyszczenia ekranu
+     */
+    public void clearScreen() {
+        setDrawingCacheEnabled(false);
+        onSizeChanged(getWidth(), getHeight(), getWidth(), getHeight());
+        invalidate();
+        setDrawingCacheEnabled(true);
+    }
+
+    /**
+     * Getter dla bitmapy wykorzystywany do save/restoreInstanceState w mainActivity
+     * @return
+     */
+    public Bitmap getmBitmap() {
+        return mBitmap;
+    }
+
+    /**
+     * Setter dla bitmapy wykorzystywany do save/restoreInstanceState w mainActivity
+     * @param mBitmap
+     */
+    public void setmBitmap(Bitmap mBitmap) {
+        this.mBitmap = mBitmap;
     }
 }
--- app/src/main/java/com/example/ism/MainActivity.java
+++ app/src/main/java/com/example/ism/MainActivity.java
@@ -1,31 +1,129 @@
 package com.example.ism;
 
 import android.app.Activity;
-import android.content.Context;
 import android.graphics.Bitmap;
-import android.graphics.Canvas;
 import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Path;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MotionEvent;
+import android.support.constraint.ConstraintLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.text.Layout;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 public class MainActivity extends Activity {
 
-    private DrawView drawView;
+    private DrawView drawView; // pole zawierające widok do rysowania
 
+    ConstraintLayout cLayout;
+    LinearLayout lLayout;
+    Bitmap bmp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        setContentView(R.layout.activity_main);
+        cLayout = findViewById(R.id.cLayout);
+
+        lLayout = new LinearLayout(this);
+
+        // utworzenie i dodanie DrawView jako widoku w constraintLayout
         drawView = new DrawView(this);
-        setContentView(drawView);
+        cLayout.addView(drawView);
+
+        addButtons();
+
+    }
+
+    public void addButtons() {
+        lLayout = new LinearLayout(this);
+
+        LinearLayout.LayoutParams param = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                1.0f
+        );
+
+
+        Button buttonRed = new Button(this);
+        Button buttonBlue = new Button(this);
+        Button buttonYellow = new Button(this);
+        Button buttonGreen = new Button(this);
+        Button buttonClear = new Button(this);
+
+        buttonRed.setLayoutParams(param);
+        buttonBlue.setLayoutParams(param);
+        buttonYellow.setLayoutParams(param);
+        buttonGreen.setLayoutParams(param);
+        buttonClear.setLayoutParams(param);
+
+        buttonRed.setBackgroundColor(Color.RED);
+        buttonBlue.setBackgroundColor(Color.BLUE);
+        buttonYellow.setBackgroundColor(Color.YELLOW);
+        buttonGreen.setBackgroundColor(Color.GREEN);
+        buttonClear.setText("X");
+
+        buttonRed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawView.setPaintColor("red");
+            }
+        });
+
+        buttonBlue.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawView.setPaintColor("blue");
+            }
+        });
+
+        buttonYellow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawView.setPaintColor("yellow");
+            }
+        });
+
+        buttonGreen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawView.setPaintColor("green");
+            }
+        });
+
+        buttonClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                drawView.clearScreen();
+            }
+        });
+
+        lLayout.addView(buttonRed);
+        lLayout.addView(buttonBlue);
+        lLayout.addView(buttonYellow);
+        lLayout.addView(buttonGreen);
+        lLayout.addView(buttonClear);
+
+
+        cLayout.addView(lLayout);
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+
+        outState.putParcelable("bitmap", drawView.getmBitmap());
+        super.onSaveInstanceState(outState);
 
 
     }
 
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
 
+        Bitmap bmp = savedInstanceState.getParcelable("bitmap");
+        drawView.setmBitmap(bmp);
+        drawView.restored = true;
+        super.onRestoreInstanceState(savedInstanceState);
+    }
 }
